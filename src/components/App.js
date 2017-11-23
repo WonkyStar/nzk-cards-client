851240af3a33dd17c74112f64822ba10f01205da
Paul Hutson <paulhutson@Pauls-MacBook-Air-2.local>
2017-11-23 17:27:13 +0000

    onClick event handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
 require('../styles/app.css')
 import React, { Component } from 'react'
+import Card from './Card'
 
 export default class App extends Component {
+
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        items: []
+      }
+    }
+
+  addCard: function() {
+      this.setState({
+          items: this.state.items.push({})
+      });
+  },
+
   render() {
     return (
       <div id='app'>
         <div id='app-bg' />
         <div id='app-inner'>
         <div id='add-card-container'>
-          <div id='add-card-button'>
+          <div id='card-list'>
+            {this.state.items.forEach()
+            }
+          </div>
+          <div id='add-card-button' onClick={this.addCard.bind(this)}>
             +
           </div>
         </div>
